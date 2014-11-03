d71ee3f1049358a6edbaf09b63b1ec5820ade39a
Tony <tony@aplawrence.com>
2014-11-03 11:53:15 -0500

    Crashing on view controller.swift

---
+++ Taskit/AddTaskViewController.swift
@@ -0,0 +1,79 @@
+//
+//  AddTaskViewController.swift
+//  Taskit
+//
+//  Created by Tony on 11/2/14.
+//  Copyright (c) 2014 A.P. Lawrence. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class AddTaskViewController: UIViewController {
+    
+   
+
+    @IBOutlet weak var addTaskTextField: UITextField!
+
+    @IBOutlet weak var addSubTaskTextField: UITextField!
+    
+    @IBOutlet weak var dueDatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    @IBAction func cancelButtonTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addTaskButtonTapped(sender: UIButton) {
+        
+        let appDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        
+        let managedObjectContext = appDelegate.managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("TaskModel", inManagedObjectContext: managedObjectContext!)
+        let task = TaskModel(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+        task.task = taskTextField.text
+        task.subtask = subtaskTextField.text
+        task.date = dueDatePicker.date
+        task.completed = false
+        
+        appDelegate.saveContext()
+        
+        var request = NSFetchRequest(entityName: "TaskModel")
+        var error:NSError? = nil
+        
+        var results:NSArray = managedObjectContext!.executeFetchRequest(request, error: &error)!
+        
+        for res in results {
+            println(res)
+        }
+        
+
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
